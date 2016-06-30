8be45b951d29101f6d1642bef1097f2f2cb2101d
Hema.Lambert <lamberthema@gmail.com>
2016-06-30 19:50:30 +0200

    notify + slider + skin

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,8 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-var apiAddress = "http://192.168.1.41/tournamentmanager/web/app_dev.php/";
+//var apiAddress = "http://192.168.1.41/tournamentmanager/web/app_dev.php/";
+var apiAddress = "http://164.132.110.56/app_dev.php/";
 
 var app = angular.module('starter', ['ionic', 'LocalStorageModule']);
 
@@ -22,8 +23,6 @@ app.run(function($ionicPlatform, $http, localStorageService) {
       StatusBar.styleDefault();
     }
   });
-
-  //$http.defaults.headers.common.Authorization = 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXUyJ9.eyJleHAiOjE0NjcxODc1NDQsInVzZXJuYW1lIjoiQW5nZWxldHRlUGFpZW1lbnRAZGF5cmVwLmNvbSIsImlhdCI6IjE0NjcxMDExNDQifQ.KcexaU6guT6baZYAp6w2-TTnd8QrBTCFJvMgynVAbQNrMj0OAU47gIveRhFDnVMaGgjvGR24wJGENCpbNmKtsIhtl5nP8qwm9VlTFP30Af4rh2riPkB_CqBMgUsbk8pHM9xJXQIK1rVZDnPiws1l3BRnT5E6oA2S2SaKm3vplxLv8-z9s4iRmoWpV2LCN6jyv2eompbz3QUnBt_eY6Rpwm18lHGhL8cSfZkS5cgG75hKeqybUKyhqfzd_5RjH1j0AGU8fqL6bAeSBJ5h0sTYly-q7aTwjpdY25d5FI_gaPc96w_gKJE0JAhYdP7WsK_cJSOE7htblU-g07OT-ov4x8eYynQMjM_J5Irjx7O7ySCKTr7BlYXmeYTy_HVwBtD1tXIWyRX2iKpgrDVGkcWhg3MVduRJd8Pk8I4NTWZvKRn7Pneq5uhjtHStuA7e6LFaP5jDCRs5skGSOHTGn1LNWcEjm06BHrEMJGXJs1_Y8rB7B9ljUn16keu-DWihCl9iqxFaW1hj0gC3vRfFxD4jhc0ekwnLn9VVS2-XZmyf9L00YrbS11BOj-0Ytq6RKbGh6sYaRuyT_qAofxwtRfQcYP0zi2YBYYvGp8BziP97b8XV1JKiPPAnTikKrgvZ7F5v9JA2cyTNO1SAiowrtelcMomSlFb_pPZAVSOIoFzGm70';
 });
 
 app.config(function ($httpProvider) {
@@ -81,16 +80,8 @@ app.config(function (localStorageServiceProvider) {
     .setPrefix('tournamentmanager');
 });
 
-app.controller('AppCtrl', ['$scope', '$rootScope','$ionicModal', '$timeout', 'localStorageService', '$http', function($scope, $rootScope, $ionicModal, $timeout, localStorageService, $http) {
+app.controller('AppCtrl', ['$scope', '$rootScope','$ionicModal', '$timeout', 'localStorageService', '$http', '$state', function($scope, $rootScope, $ionicModal, $timeout, localStorageService, $http, $state) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
   $scope.loginData = {};
 
   $rootScope.apiAddress = apiAddress;
@@ -121,7 +112,9 @@ app.controller('AppCtrl', ['$scope', '$rootScope','$ionicModal', '$timeout', 'lo
         url : $rootScope.apiAddress+"login_check",
         data: $scope.loginData
     })
+  
     .then(function mySucces(response) {
+       console.log(response);
        localStorageService.set("access_token", response.data.token);
     }, function myError(response) {
        console.log(response);
@@ -142,19 +135,82 @@ app.controller('AppCtrl', ['$scope', '$rootScope','$ionicModal', '$timeout', 'lo
     },500);
   }
 
-  //$scope.$on('$home.enter', function(e) {
-    $rootScope.access_token = localStorageService.get("access_token");
-    $http({
-        method : "GET",
-        url : $rootScope.apiAddress+"me",
+  // if(!localStorageService.get("access_token")){
+  //   $scope.login();
+  // }
+
+    if(localStorageService.get("access_token")){
+      $rootScope.access_token = localStorageService.get("access_token");
+      $http({
+          method : "GET",
+          url : $rootScope.apiAddress+"me",
+          headers: {
+             'Authorization':  "Bearer "+ $rootScope.access_token
+           }
+      })
+      .then(function mySucces(response) {
+         $scope.user = response.data;
+
+         var debut = new Date().getFullYear();
+         var birth_date = new Date($scope.user.birth_date).getFullYear();
+         $scope.age = debut - birth_date;
+         
+      }, function myError(response) {
+         console.log(response);
+      });
+
+      $scope.notification = {
+      unread : false
+    }
+
+    var notif_id;
+
+    function checkNotif(){
+      $http({
+        method: 'GET',
+        url: $rootScope.apiAddress+'notification/last',
         headers: {
-           'Authorization':  "Bearer "+ $rootScope.access_token
-         }
-    })
-    .then(function mySucces(response) {
-       $scope.user = response.data;
-    }, function myError(response) {
-       console.log(response);
+             'Authorization':  "Bearer "+ $rootScope.access_token
+           }
+      }).then(function successCallback(r) {
+        console.log("Notif");
+        if(r.data.length > 0){
+          $scope.notification.data = r.data[0];
+          $scope.notification.unread = true;
+          notif_id = $scope.notification.data.tournament.id;
+          console.log(r);
+          console.log($scope.notification);
+          console.log(notif_id);
+        }
+      }, function errorCallback(r) {
+          console.log("Unable to check for notification");
+      });
+    }
+
+
+    $scope.notification.view = function(notification){
+      $scope.notification.unread = false;
+      $http({
+        method: 'POST',
+        url: $rootScope.apiAddress+'notification/'+notification+'/seen',
+        headers: {
+             'Authorization':  "Bearer "+ $rootScope.access_token
+           }
+      }).then(function successCallback(r) {
+        console.log("Notification set as read");
+        $state.go('app.singleTournament',{id : notif_id});
+      }, function errorCallback(r) {
+          console.log("Unable to set notification as read");
+      });
+    }
+
+    //initialize Bootstrap components
+    $(function () {
+      //check notif on load and every 15 sec
+      checkNotif();
+      var r = setInterval(function(){
+        checkNotif();
+      },15000);
     });
-  //});
+  }
 }]);
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,7 +13,8 @@ app.controller('HomeCtrl', ['$scope', '$rootScope', '$http', function($scope, $r
     });
 }]);
 
-app.controller('TournamentCtrl', ['$scope', '$rootScope', '$http', '$stateParams', function($scope, $rootScope, $http, $stateParams) {
+app.controller('TournamentCtrl', ['$scope', '$rootScope', '$http', '$stateParams', '$ionicSlideBoxDelegate', function($scope, $rootScope, $http, $stateParams, $ionicSlideBoxDelegate) {
+  var round_nb;
   $http({
       method : "GET",
       url : $rootScope.apiAddress+"tournaments/"+$stateParams.tournamentId,
@@ -25,9 +26,79 @@ app.controller('TournamentCtrl', ['$scope', '$rootScope', '$http', '$stateParams
      $scope.tournament = response.data;
      $scope.players = $scope.tournament.accounts;
 
-     var date = new Date($scope.tournament.date_begin);
-     date = date.toDateString();
+     $scope.date = new Date($scope.tournament.date_begin);
+     $scope.date = $scope.date.toLocaleString();
+     console.log(response.data);
+
   }, function myError(response) {
      console.log(response);
   });
+
+  $http({
+      method : "GET",
+      url : $rootScope.apiAddress+"tournaments/"+$stateParams.tournamentId+"/battles",
+      headers: {
+         'Authorization':  "Bearer "+ $rootScope.access_token
+       }
+  })
+  .then(function mySucces(response) {
+    var battles = [];
+    battles = response.data;
+    var battles_nb = battles.length;
+    $scope.round_nb = battles[Object.keys(battles)[Object.keys(battles).length - 1]].round;
+    var battles_by_round = {};
+
+    for (var i = 1; i <= $scope.round_nb; i++) {
+      battles_by_round[i] = getBattlesbyRound(battles, i);
+    }
+
+    $scope.battles_by_round = battles_by_round;
+
+    console.log(battles);
+
+    console.log(battles_by_round);
+    round_nb = $scope.round_nb;
+
+    var window_width = $(window).width();
+    //$(".slider-slides").css("width", $scope.round_nb * window_width);
+    //$(".slider-slides ion-slide").css("width", window_width);
+    //$("ion-scroll").css("width", window_width);
+
+  }, function myError(response) {
+     console.log(response);
+  });
+
+  // var block_height = $(".round_block").height();
+  // $(".round_block").css("line-height", block_height+"px");
+
+  $(".ion-chevron-right").click(function(){
+    window.scrollTo("#round2");
+  });
+
+  //SLIDER
+  $scope.next = function() {
+    $ionicSlideBoxDelegate.next();
+  };
+  $scope.previous = function() {
+    $ionicSlideBoxDelegate.previous();
+  };
+
+  // Called each time the slide changes
+  $scope.slideChanged = function(index) {
+    $scope.slideIndex = index;
+  };
+
+  function getBattlesbyRound(battles, round){
+    var array_battles = [];
+    battles.forEach(function(element){
+      if(element.round == round){
+        array_battles.push(element);
+      }
+    });
+
+    return array_battles;
+  }
+
 }]);
+
+
